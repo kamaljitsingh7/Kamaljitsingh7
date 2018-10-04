984e37c35e66d973b7ad65d1ca765c0a49548c14
Gunjan Chanana <gc.chanana@gmail.com>
2018-10-04 11:58:49 +0530

    Loading profile data based on URL

--- src/app.js
+++ src/app.js
@@ -1,32 +1,70 @@
-
 export class App {
-  constructor() {
-    //give print icon (with all expanded)
+    constructor() {
+        //give print icon (with all expanded)
 
-    this.pageLabels = {
-      Summary: 'Summary',
-      Experience: 'Experience',
-      Education: 'Education',
-      OtherDetails: 'Other Details',
-      LastUpdatedOn: 'Last Updated On : ',
-      TechSkillHeaderObj: {
-        Type: 'tag',
-        Heading: 'Technical Skills'
-      },
-      Client: 'Client',
-      Server: 'Server',
-      Database: 'Database',
-      Others: 'Others',
-      TechSkillFooter: '* denotes learning'
+        this.pageLabels = {
+            Summary: 'Summary',
+            Experience: 'Experience',
+            Education: 'Education',
+            OtherDetails: 'Other Details',
+            LastUpdatedOn: 'Last Updated On : ',
+            TechSkillHeaderObj: {
+                Type: 'tag',
+                Heading: 'Technical Skills'
+            },
+            Client: 'Client',
+            Server: 'Server',
+            Database: 'Database',
+            Others: 'Others',
+            TechSkillFooter: '* denotes learning'
 
-    };
-  }
-  activate() {
-    //can fetch profile_data user path dynamically using location url
-    var promObj = import('./data/prasankumarn/profile_data').then((module) => {
-      this.pageData = module.profileData;
-      return Promise.resolve(this.pageData);
-    });
-    return promObj;
-  }
+        };
+    }
+    activate() {
+        var promObj = this.getProfileDataByUrl();
+        return promObj;
+    }
+
+    getProfileDataByUrl() {
+        var currentUrl = window.location.href;
+        var userName = '';
+      
+        if(currentUrl.indexOf("localhost") < 0) {
+            var initialIndex = currentUrl.indexOf('//');
+            var finalIndex = currentUrl.indexOf('.github.io');
+            if(initialIndex && finalIndex) {
+                userName = currentUrl.slice(initialIndex+1, finalIndex);
+            }
+        }
+
+        var prmObj = new Promise((resolve, reject)=>{
+
+            var path = './data' + userName + '/profile_data';
+            var promObj1 = this.getPageData(path);
+
+            promObj1.then((pageData) => {
+                resolve(pageData);
+            }).catch((error) => {
+                path = './data/default/profile_data';
+
+                this.getPageData(path).then((pageData)=>{
+                    resolve(pageData);
+                }).catch((error)=>{
+                    reject(error);
+                });
+            });
+          
+        });
+        return prmObj;
+    }
+
+    getPageData(_path) {
+        var promObj = import('' + _path).then((module) => {
+            this.pageData = module.profileData;
+            return Promise.resolve(module.profileData);
+        }, (error) => {
+            return Promise.reject(error);
+        });
+        return promObj;
+    }
 }
