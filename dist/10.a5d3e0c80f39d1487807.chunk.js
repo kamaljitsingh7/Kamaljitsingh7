0313556b527c8e67299310780a8e84684663eb1c
Gunjan Chanana <gc.chanana@gmail.com>
2018-10-08 15:15:05 +0530

    Build Applied

--- dist/10.a5d3e0c80f39d1487807.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{131:function(e,n){throw new Error("Module parse failed: Unexpected token (1:1)\nYou may need an appropriate loader to handle this file type.\n> \ufeff.sha-pg008-02 {\r\n|     --tag-dot-height-width: 10px;\r\n|     --tag-ring-height: 10px;\r")}}]);
-//# sourceMappingURL=10.a5d3e0c80f39d1487807.bundle.map
